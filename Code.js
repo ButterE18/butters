385c2c4283842b0aa808fb1f3ae4d2e144e4eb33
ButterE18 <113061246+ButterE18@users.noreply.github.com>
2022-09-21 13:17:47 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,64 @@
+function runTimer(x){ //This function runs on the countdown
+
+ var currTime = 50;
+ var Timeout = 5000;
+ 
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+     x.innerHTML = currTime
+     currTime = currTime - 5;
+ }, Timeout);
+ Timeout = Timeout + 1000;
+ setTimeout(function(){
+ alert('SIKE, Still no cookies for you, lmao XD!');
+ }, Timeout);
+}
