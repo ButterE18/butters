8554b253bb960a28c2044a295f2e1be5aa8861b5
ButterE18 <113061246+ButterE18@users.noreply.github.com>
2022-09-28 13:22:53 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -2,63 +2,17 @@ function runTimer(x){ //This function runs on the countdown
 
  var currTime = 50;
  var Timeout = 5000;
- 
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
-     x.innerHTML = currTime
-     currTime = currTime - 5;
- }, Timeout);
- Timeout = Timeout + 1000;
- setTimeout(function(){
- alert('SIKE, Still no cookies for you, lmao XD!');
- }, Timeout);
-}
+    
+for(i=0;i<11;i++){
+        
+    setTimeout(function(){    
+        if(currTime == 25){
+        alert("HALFWAY THERE");
+
+        x.innerHTML = currTime
+        currTime = currTime - 5;
+    },Timeout);
+
+    Timeout = Timeout + 1000;
+    }
+}    
