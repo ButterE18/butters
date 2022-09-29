576a7c15ec3fe6622e5e02fb07c710cb0ac235f0
ButterE18 <113061246+ButterE18@users.noreply.github.com>
2022-09-29 15:37:19 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -5,14 +5,21 @@ function runTimer(x){ //This function runs on the countdown
     
 for(i=0;i<11;i++){
         
-    setTimeout(function(){    
-        if(currTime == 25){
-        alert("HALFWAY THERE");
+    setTimeout(function(){
+        if(currTime == 0){
+        x.innerHTML = currTime  
+        alert("SIKE still no cookies for you lmao"); //this alert shows once the timer hits 0
 
-        x.innerHTML = currTime
+        }    
+        else if(currTime < 25){
+            x.innerHTML = "Warning Less than half way to launch, time left = "  + currTime //this displays a message that appears when timer is less than 25
+        }
+        else{
+            x.innerHTML = currTime
+        }
         currTime = currTime - 5;
     },Timeout);
-
+        
     Timeout = Timeout + 1000;
     }
 }    
