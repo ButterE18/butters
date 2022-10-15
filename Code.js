741b4e15dba375c4d884579632523d4ed50952d6
ButterE18 <113061246+ButterE18@users.noreply.github.com>
2022-10-15 03:26:54 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,51 +1,51 @@
 function runTimer(x){ //This function runs on the countdown
 
- var currTime = 50;
- var Timeout = 5000;
-    
-for(i=0;i<11;i++){
-        
-    setTimeout(function(){
-        if(currTime == 0){
-        x.innerHTML = currTime  
-        alert("SIKE still no cookies for you lmao"); //this alert shows once the timer hits 0
-
-        }    
-        else if(currTime < 25){
-            x.innerHTML = "Warning Less than half way to launch, time left = "  + currTime //this displays a message that appears when timer is less than 25
-        }
-        else{
-            x.innerHTML = currTime
-        }
-        currTime = currTime - 5;
-    },Timeout);
-        
-    Timeout = Timeout + 1000;
-    }
-}    
-
-function getInput() {
-
-do{
-    Firstname = prompt("GIVE ME FIRST NAME PWEASE:");
-    Lastname = prompt("GIVE ME LAST NAME NOW PWEASE:");
-
-    Fullname = Firstname + " " + Lastname; 
-        if(Fullname.length > 20){
-            alert("YOUR NAME IS TOO BIG, CHANGE IT PWEASE");
-        }
-}
-while(Fullname.length > 20);
-
-do{
-    BadgeHashtag = prompt("TYPE IN SPECIALIZED NUMERO PWEASE:");
-
-    if(BadgeHashtag > 1000){
-        alert("ERROR DOES NOT COMPUTE LMAO");
-    }
-}
-while(BadgeHashtag > 1000);
-
-
-
-}
+    var currTime = 50;
+    var Timeout = 5000;
+       
+   for(i=0;i<11;i++){
+           
+       setTimeout(function(){
+           if(currTime == 0){
+           x.innerHTML = currTime  
+           alert("SIKE still no cookies for you lmao"); //this alert shows once the timer hits 0
+   
+           }    
+           else if(currTime < 25){
+               x.innerHTML = "Warning Less than half way to launch, time left = "  + currTime //this displays a message that appears when timer is less than 25
+           }
+           else{
+               x.innerHTML = currTime
+           }
+           currTime = currTime - 5;
+       },Timeout);
+           
+       Timeout = Timeout + 1000;
+       }
+   }    
+   
+   function getInput() { 
+   
+   do{
+       Firstname = prompt("GIVE ME FIRST NAME PWEASE:");
+       Lastname = prompt("GIVE ME LAST NAME NOW PWEASE:");
+   
+       Fullname = Firstname + " " + Lastname; //This variable will be used to ensure that the combination of both names is under 1 variable
+           if(Fullname.length > 20){
+               alert("YOUR NAME IS TOO BIG, CHANGE IT PWEASE");
+           }
+   }
+   while(Fullname.length > 20); //This is how we break away from the loop if the length exceeds 20 characters
+   
+   do{
+       BadgeHashtag = prompt("TYPE IN SPECIALIZED NUMERO PWEASE:");
+   
+       if(BadgeHashtag > 1000){ //1000 ensures that the badge number is 3 numbers or less
+           alert("ERROR DOES NOT COMPUTE LMAO");
+       }
+   }
+   while(BadgeHashtag > 1000); //Just like line 38 but for the Badge number
+   
+   
+   
+   }
